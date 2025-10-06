6588eb6fbb6faaa2429bae4dc8476c1f7cc5ef32
JoaoEmanuel-arch <pnjoaoemanuel@gmail.com>
2025-10-06 14:40:59 -0300

    Refatora gestor: exclui lista de tecnicos e refatora toString

--- src/main/java/com/joao/empresa/model/Gestor.java
+++ src/main/java/com/joao/empresa/model/Gestor.java
@@ -1,13 +1,8 @@
 package main.java.com.joao.empresa.model;
 
-import java.util.ArrayList;
-import java.util.List;
-
 public class Gestor extends Usuario {
 
     private String areaResponsavel;
-
-    private List<Tecnico> equipeTecnicos = new ArrayList<Tecnico>();
 
     public Gestor(int id, String nome, String email, String senha, String areaResponsavel) {
         super(id, nome, email, senha, TipoUsuario.GESTOR);
@@ -20,14 +15,6 @@ public class Gestor extends Usuario {
 
     public void setAreaResponsavel(String areaResponsavel) {
         this.areaResponsavel = areaResponsavel;
-    }
-
-    public List<Tecnico> getEquipeTecnicos() {
-        return equipeTecnicos;
-    }
-
-    public void adicionarTecnico(Tecnico tecnico) {
-        equipeTecnicos.add(tecnico);
     }
 
     @Override
@@ -43,8 +30,7 @@ public class Gestor extends Usuario {
     public String toString() {
         return super.toString() +
                 "Gestor{" +
-                "areaResponsavel='" + areaResponsavel + '\'' +
-                ", chamadosAtivos=" + equipeTecnicos +
+                "areaResponsavel='" + areaResponsavel +
                 '}';
     }
 }
