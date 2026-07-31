be76764198c8171ea1acd93d375d57842ba3f32b
JoaoEmanuel-arch <pnjoaoemanuel@gmail.com>
2026-07-31 15:32:06 -0300

    refactor: adicionar construtor com id nulo que ainda virá do banco

--- src/main/java/com/joao/empresa/model/Administrador.java
+++ src/main/java/com/joao/empresa/model/Administrador.java
@@ -25,7 +25,13 @@ public class Administrador extends Usuario {
     private NivelAcesso nivelAcesso;
     private String departamento;
 
-    public Administrador(int id, String nome, String email, String senha, String departamento){
+    public Administrador(String nome, String email, String senha, String departamento) {
+        super(nome, email, senha, TipoUsuario.ADMINISTRADOR);
+        this.nivelAcesso = NivelAcesso.TOTAL;
+        this.departamento = departamento;
+    }
+
+    public Administrador(Integer id, String nome, String email, String senha, String departamento){
         super(id, nome, email, senha, TipoUsuario.ADMINISTRADOR);
         nivelAcesso = NivelAcesso.TOTAL;
         this.departamento = departamento;
