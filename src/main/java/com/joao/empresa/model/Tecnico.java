491d1cc0c85c2360dd5ae5918157fd2fc7711da7
JoaoEmanuel-arch <pnjoaoemanuel@gmail.com>
2026-07-31 15:26:13 -0300

    refactor: adicionar construtor com id nulo que ainda virá do banco

--- src/main/java/com/joao/empresa/model/Tecnico.java
+++ src/main/java/com/joao/empresa/model/Tecnico.java
@@ -9,7 +9,12 @@ public class Tecnico extends Usuario {
 
     private Set<Manutencao> manutencoesResponsaveis = new LinkedHashSet<>();
 
-    public Tecnico(int id, String nome, String email, String senha, String especialidade) {
+    public Tecnico(String nome, String email, String senha, String especialidade) {
+        super(nome, email, senha, TipoUsuario.TECNICO);
+        this.especialidade = especialidade;
+    }
+
+    public Tecnico(Integer id, String nome, String email, String senha, String especialidade) {
         super(id, nome, email, senha, TipoUsuario.TECNICO);
         this.especialidade = especialidade;
     }
