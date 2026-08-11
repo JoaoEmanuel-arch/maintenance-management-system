082f36a127b95378969d57abbcdb5b6b71c8cff8
JoaoEmanuel-arch <pnjoaoemanuel@gmail.com>
2026-08-11 01:12:35 -0300

    refactor: adicionar mais um construtor com a causa

--- src/main/java/com/joao/empresa/exceptions/UsuarioJaCadastradoException.java
+++ src/main/java/com/joao/empresa/exceptions/UsuarioJaCadastradoException.java
@@ -2,8 +2,14 @@ package com.joao.empresa.exceptions;
 
 public class UsuarioJaCadastradoException extends RuntimeException {
 
-    public UsuarioJaCadastradoException(String msg) {
-        super(msg);
+    public UsuarioJaCadastradoException(String mensagem) {
+        super(mensagem);
     }
 
+    public UsuarioJaCadastradoException(
+            String mensagem,
+            Throwable causa
+    ) {
+        super(mensagem, causa);
+    }
 }
