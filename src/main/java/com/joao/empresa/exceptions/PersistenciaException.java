6563073bb4dd6f0150d6497d36b9a512166884de
JoaoEmanuel-arch <pnjoaoemanuel@gmail.com>
2026-08-11 00:16:16 -0300

    docs: adicionar comentário

--- src/main/java/com/joao/empresa/exceptions/PersistenciaException.java
+++ src/main/java/com/joao/empresa/exceptions/PersistenciaException.java
@@ -1,6 +1,6 @@
 package com.joao.empresa.exceptions;
 
-// erro de banco
+// Alguma operação da camada de persistência falhou
 public class PersistenciaException extends RuntimeException {
     public PersistenciaException(String message,  Throwable causa) {
         super(message, causa);
