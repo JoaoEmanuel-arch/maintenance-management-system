13ac1139ed45d7c025b4325f6c1102cdedc4162e
JoaoEmanuel-arch <pnjoaoemanuel@gmail.com>
2026-08-10 23:12:42 -0300

    docs: adicionar comentário

--- src/main/java/com/joao/empresa/exceptions/PersistenciaException.java
+++ src/main/java/com/joao/empresa/exceptions/PersistenciaException.java
@@ -1,5 +1,6 @@
 package com.joao.empresa.exceptions;
 
+// erro de banco
 public class PersistenciaException extends RuntimeException {
     public PersistenciaException(String message,  Throwable causa) {
         super(message, causa);
