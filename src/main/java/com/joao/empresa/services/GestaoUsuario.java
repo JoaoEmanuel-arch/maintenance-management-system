1f5a8f0866202f71cb5e7c29bc882f4acc532cb6
JoaoEmanuel-arch <pnjoaoemanuel@gmail.com>
2026-08-12 16:49:48 -0300

    refactor: traduz exceção de registro duplicado para UsuarioJaCadastradoException

--- src/main/java/com/joao/empresa/services/GestaoUsuario.java
+++ src/main/java/com/joao/empresa/services/GestaoUsuario.java
@@ -5,6 +5,17 @@ import com.joao.empresa.model.*;
 import com.joao.empresa.exceptions.*;
 import java.util.*;
 
+/* Traduzir erros técnicos do DAO para erros que fazem sentido para o negócio
+
+
+* MySQL 1062 -> SQLException -> TradutorSQLException -> RegistroDuplicadoException ->
+GestaoUsuario -> UsuarioJaCadastradoException
+
+* MySQL bloqueia FK -> IntegridadeReferencialException -> GestaoUsuario ->
+EntidadeEmUsoException
+
+ */
+
 public class GestaoUsuario {
 
     private UsuarioDAO usuarioDAO = new UsuarioDAO(); // poder mexer no banco daqui mesmo
@@ -19,8 +30,21 @@ public class GestaoUsuario {
         return usuario;
     }
 
-    public void cadastrarUsuario(Usuario usuario){
-        usuarioDAO.salvar(usuario);
+    public void cadastrarUsuario(Usuario usuario) {
+
+        // antes terminava em erro técnico, agora se o DAO lançar exceção eu trato aqui
+        try {
+
+            usuarioDAO.salvar(usuario);
+
+        // vou especificando cada vez mais o erro, deixando de ser genérico
+        } catch (RegistroDuplicadoException e) { // linguagem da persistência
+
+            throw new UsuarioJaCadastradoException( // linguagem de domínio
+                    "Já existe um usuário cadastrado com o e-mail "
+                            + usuario.getEmail() + ".", e // preservo a exceção anterior como causa
+            );
+        }
     }
 
     public List<Usuario> listarUsuarios() {
