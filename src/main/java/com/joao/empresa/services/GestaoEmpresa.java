3b42a27ccfe2212160febe25c3aae1731c784080
JoaoEmanuel-arch <pnjoaoemanuel@gmail.com>
2026-08-13 18:39:19 -0300

    feat: tratar erro de empresa ja cadastrada

--- src/main/java/com/joao/empresa/services/GestaoEmpresa.java
+++ src/main/java/com/joao/empresa/services/GestaoEmpresa.java
@@ -1,17 +1,20 @@
 package com.joao.empresa.services;
 
 import com.joao.empresa.dao.EmpresaDAO;
+import com.joao.empresa.exceptions.EmpresaJaCadastradaException;
 import com.joao.empresa.exceptions.EmpresaNaoEncontradaException;
+import com.joao.empresa.exceptions.RegistroDuplicadoException;
 import com.joao.empresa.model.Empresa;
 import java.util.List;
 
 public class GestaoEmpresa {
 
-    EmpresaDAO empresaDAO = new EmpresaDAO();
+    EmpresaDAO empresaDAO = new EmpresaDAO(); //faço isso aqui só pra usar os métodos dessa classe
 
     public Empresa buscarPorId(int id) {
         Empresa empresa = empresaDAO.buscarPorId(id);
 
+        // esse caso de erro aqui é responsabilidade somente daqui de tratar
         if(empresa == null){
             throw new EmpresaNaoEncontradaException("Empresa com ID " + id + " não encontrada.");
         }
@@ -19,8 +22,24 @@ public class GestaoEmpresa {
         return empresa;
     }
 
-    public void cadastrarEmpresa(Empresa empresa){
-        empresaDAO.salvar(empresa);
+    public void cadastrarEmpresa(Empresa empresa) {
+
+        if (empresa == null) {
+            throw new IllegalArgumentException("A empresa a ser cadastrada não pode ser nula.");
+        }
+
+        try {
+
+            empresaDAO.salvar(empresa);
+
+        } catch (RegistroDuplicadoException e) {
+            //recebo o erro que traduziu do banco, mas especifico ainda mais, mantendo a causa original
+            throw new EmpresaJaCadastradaException(
+                    "Já existe uma empresa cadastrada com o CNPJ "
+                            + empresa.getCnpj()
+                            + ".", e
+            );
+        }
     }
 
     public List<Empresa> listarEmpresas(){
