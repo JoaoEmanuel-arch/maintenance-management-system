1674bbc24dbfd168445c2a137ccecf0cdcfc32ce
JoaoEmanuel-arch <pnjoaoemanuel@gmail.com>
2026-08-14 08:59:36 -0300

    refactor: alterar construtor da exceção para pegar a causa original

--- src/main/java/com/joao/empresa/exceptions/EmpresaNaoEncontradaException.java
+++ src/main/java/com/joao/empresa/exceptions/EmpresaNaoEncontradaException.java
@@ -1,8 +1,12 @@
 package com.joao.empresa.exceptions;
 
-public class EmpresaNaoEncontradaException extends RuntimeException{
+public class EmpresaNaoEncontradaException extends RuntimeException {
 
-    public EmpresaNaoEncontradaException(String mensagem){
+    public EmpresaNaoEncontradaException(String mensagem) {
         super(mensagem);
+    }
+
+    public EmpresaNaoEncontradaException(String mensagem, Throwable causa) {
+        super(mensagem, causa);
     }
 }
