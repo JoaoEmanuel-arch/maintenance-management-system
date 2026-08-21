e6459a37539a4069d799630c1aeb77777ffcc86a
JoaoEmanuel-arch <pnjoaoemanuel@gmail.com>
2026-08-21 00:26:06 -0300

    delete: excluir imports obsoletos

--- src/test/java/com/joao/empresa/services/GestaoEquipamentoTest.java
+++ src/test/java/com/joao/empresa/services/GestaoEquipamentoTest.java
@@ -6,11 +6,7 @@ import com.joao.empresa.dao.ManutencaoDAO;
 import com.joao.empresa.exceptions.*;
 import com.joao.empresa.model.Equipamento;
 import org.junit.jupiter.api.*;
-import org.junit.jupiter.api.extension.ExtendWith;
-import org.mockito.InjectMocks;
 import org.mockito.Mock;
-import org.mockito.junit.jupiter.MockitoExtension;
-import java.time.LocalDate;
 import java.util.List;
 
 import static org.junit.jupiter.api.Assertions.*;
