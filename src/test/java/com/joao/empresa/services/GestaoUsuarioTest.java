e383e201c0590995c53b7ff9b52a564ce9b231c1
JoaoEmanuel-arch <pnjoaoemanuel@gmail.com>
2026-08-21 08:50:03 -0300

    delete: deleção completa dos testes e refatoração do zero

--- src/test/java/com/joao/empresa/services/GestaoUsuarioTest.java
+++ src/test/java/com/joao/empresa/services/GestaoUsuarioTest.java
@@ -1,76 +1,33 @@
 package com.joao.empresa.services;
 
 import com.joao.empresa.builders.AdministradorBuilder;
+import com.joao.empresa.builders.GestorBuilder;
+import com.joao.empresa.builders.TecnicoBuilder;
+import com.joao.empresa.dao.UsuarioDAO;
+import com.joao.empresa.exceptions.EntidadeEmUsoException;
+import com.joao.empresa.exceptions.IntegridadeReferencialException;
+import com.joao.empresa.exceptions.RegistroDuplicadoException;
 import com.joao.empresa.exceptions.UsuarioJaCadastradoException;
 import com.joao.empresa.exceptions.UsuarioNaoEncontradoException;
+import com.joao.empresa.model.Administrador;
+import com.joao.empresa.model.Gestor;
+import com.joao.empresa.model.Tecnico;
 import com.joao.empresa.model.Usuario;
-import org.junit.jupiter.api.*;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
+
+import java.util.List;
 
 import static org.junit.jupiter.api.Assertions.*;
-
-/* ---------------------------------------------------------------------------------------------------------------
-* Para os métodos cadastrarUsuario, buscarPorId, listarUsuarios e removerUsuario vou testar com um subtipo qualquer
-* de Usuario. Esses métodos são agnósticos de tipo, então para qualquer um que eu testar serve para todos. Porém,
-* para o método atualizarUsuario eu vou precisar testar para cada subtipo, porque cada um tem regra própria,
-* cada um altera campos diferentes e cada um reage diferente ao alterado.
-------------------------------------------------------------------------------------------------------------------*/
+import static org.mockito.Mockito.*;
 
 public class GestaoUsuarioTest {
 
-    // atributos disponíveis para todas as classes, se criasse no before each ia morrer ali mesmo
-    private GestaoUsuario gestaoUsuario;
-    private Usuario usuarioNovo;
 
-    @BeforeEach
-    public void antesDeCadaMetodoInstanciaOsObjetos() {
-        gestaoUsuario = new GestaoUsuario();
-        usuarioNovo = AdministradorBuilder.builder().comId(1).build(); // deixar explícito no teste que o id é 1
-    }
-
-    @Test
-    public void quandoMetodoBuscarPorIdForChamadoEExistirUsuarioComIdBuscadoDeveRetornarEsseUsuario(){
-        gestaoUsuario.cadastrarUsuario(usuarioNovo);
-
-        Usuario usuario = gestaoUsuario.buscarPorId(1);
-
-        assertEquals(1, usuario.getId());
-    }
-
-    @Test
-    public void quandoMetodoBuscarPorIdForChamadoENaoExistirUsuarioComOIdBuscadoDeveLancarExcecao(){
-
-        assertThrows(UsuarioNaoEncontradoException.class, () ->{
-            gestaoUsuario.buscarPorId(2);
-        });
-    }
-
-    @Test
-    public void quandoOMetodoCadastrarUsuarioForChamadoSeJaExistirUmUsuarioComOMesmoIdDeveLancarExcecao(){
-
-        Usuario usuarioNovo2 = AdministradorBuilder.builder().build();
-        gestaoUsuario.cadastrarUsuario(usuarioNovo);
-
-        assertThrows(UsuarioJaCadastradoException.class, () -> {
-            gestaoUsuario.cadastrarUsuario(usuarioNovo2);
-        });
-    }
-
-    @Test
-    public void quandoCadastrarUsuarioForChamadoSemConflitosDeIdDeveAdicionarUsuarioAoSistema(){
-
-        gestaoUsuario.cadastrarUsuario(usuarioNovo);
-
-        assertTrue(gestaoUsuario.listarUsuarios().contains(usuarioNovo));
-    }
-
-    @Test
-    public void quandoOMetodoExcluirUsuarioForChamadoOUsuarioDeveSerExcluidoDoSistema(){
-
-        gestaoUsuario.cadastrarUsuario(usuarioNovo);
-
-        gestaoUsuario.removerUsuario(1);
-
-        assertFalse(gestaoUsuario.listarUsuarios().contains(usuarioNovo));
-    }
 
 }
