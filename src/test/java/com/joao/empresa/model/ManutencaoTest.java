5fcd343aa67cad74e8038f9339ba87233fa9df0a
JoaoEmanuel-arch <pnjoaoemanuel@gmail.com>
2026-08-24 23:30:41 -0300

    test: criar manuntenção do zero e iniciar com status em andamento

---
+++ src/test/java/com/joao/empresa/model/ManutencaoTest.java
@@ -0,0 +1,57 @@
+package com.joao.empresa.model;
+
+import com.joao.empresa.builders.EquipamentoBuilder;
+import com.joao.empresa.builders.TecnicoBuilder;
+import org.junit.jupiter.api.Test;
+
+import java.math.BigDecimal;
+import java.time.LocalDate;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+public class ManutencaoTest {
+
+    @Test
+    void criarManutencao_quandoDadosForemValidos_deveIniciarEmAndamento() {
+
+        Equipamento equipamento =
+                EquipamentoBuilder.builder()
+                        .comId(1)
+                        .build();
+
+        Tecnico tecnico =
+                TecnicoBuilder.builder()
+                        .comId(1)
+                        .build();
+
+        Manutencao manutencao =
+                new Manutencao(
+                        Manutencao.TipoManutencao.CORRETIVA,
+                        "Troca da correia",
+                        LocalDate.of(2026, 8, 20),
+                        equipamento,
+                        tecnico
+                );
+
+        assertAll(
+                () -> assertNull(manutencao.getId()), // não chamou DAO ainda
+
+                () -> assertEquals(
+                        Manutencao.Status.ANDAMENTO,
+                        manutencao.getStatus()
+                ),
+
+                () -> assertEquals(
+                        BigDecimal.ZERO,
+                        manutencao.getCusto()
+                ),
+
+                () -> assertNull(
+                        manutencao.getDataFim() // não pode possui data final ainda
+                )
+        );
+    }
+
+
+
+}
