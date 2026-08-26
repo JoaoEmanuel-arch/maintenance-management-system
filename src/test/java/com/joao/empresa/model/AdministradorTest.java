c82bc0c093753e0286a345f81dd992044606168c
JoaoEmanuel-arch <pnjoaoemanuel@gmail.com>
2026-08-26 17:47:36 -0300

    test: adicionar teste de atualização dos dados do administrador (usuario)

--- src/test/java/com/joao/empresa/model/AdministradorTest.java
+++ src/test/java/com/joao/empresa/model/AdministradorTest.java
@@ -2,6 +2,7 @@ package com.joao.empresa.model;
 
 import org.junit.jupiter.api.Test;
 
+import static org.junit.jupiter.api.Assertions.assertAll;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
 public class AdministradorTest {
@@ -20,6 +21,39 @@ public class AdministradorTest {
         assertEquals(Usuario.TipoUsuario.ADMINISTRADOR, administrador.getTipo());
     }
 
+    @Test
+    void atualizarDados_quandoDadosForemInformados_deveAtualizarNomeEEmail() {
 
+        // isso é o método do usuário que ele herda
+        Administrador existente =
+                new Administrador(
+                        1,
+                        "João",
+                        "antigo@email.com",
+                        "Financeiro"
+                );
+
+        Administrador alterado =
+                new Administrador(
+                        1,
+                        "João Emanuel",
+                        "novo@email.com",
+                        "Tecnologia"
+                );
+
+        existente.atualizarDados(alterado); // alterado é do usuario é so nome e-mail
+
+        assertAll(
+                () -> assertEquals(
+                        "João Emanuel",
+                        existente.getNome()
+                ),
+
+                () -> assertEquals(
+                        "novo@email.com",
+                        existente.getEmail()
+                )
+        );
+    }
 
 }
