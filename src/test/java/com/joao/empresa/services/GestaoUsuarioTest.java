6e613bad642a374f51a3e51f4204e39b10ddc663
JoaoEmanuel-arch <pnjoaoemanuel@gmail.com>
2026-09-03 17:19:17 -0300

    refactor: ativar extensão do mockito na classe

--- src/test/java/com/joao/empresa/services/GestaoUsuarioTest.java
+++ src/test/java/com/joao/empresa/services/GestaoUsuarioTest.java
@@ -16,8 +16,8 @@ import com.joao.empresa.model.Usuario;
 
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
-import org.junit.jupiter.api.extension.ExtendWith;
 
+import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
 
@@ -26,6 +26,7 @@ import java.util.List;
 import static org.junit.jupiter.api.Assertions.*;
 import static org.mockito.Mockito.*;
 
+@ExtendWith(MockitoExtension.class)
 public class GestaoUsuarioTest {
 
     @Mock
